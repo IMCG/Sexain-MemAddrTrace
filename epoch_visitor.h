c6dc9203e4dbb5bc6793e533287984bf7afc20f6
Jinglei Ren <jinglei.ren@gmail.com>
2014-01-18 19:28:27 -0500

    Add intermediate class PageVisitor.

--- epoch_visitor.h
+++ epoch_visitor.h
@@ -28,35 +28,65 @@ class BlockCountVisitor : public EpochVisitor {
   uint64_t count_;
 };
 
-class DirtyRatioVisitor : public EpochVisitor {
+class PageVisitor : public EpochVisitor {
+ public:
+  PageVisitor(int page_bits);
+  virtual void Visit(const BlockSet& blocks);
+  int page_bits() const { return page_bits_; }
+  int page_blocks() const { return page_blocks_; }
+  int num_visits() const { return num_visits_; }
+ private:
+  const int page_bits_;
+  const int page_blocks_;
+  int num_visits_;
+};
+
+class DirtyRatioVisitor : public PageVisitor {
  public:
   DirtyRatioVisitor(int page_bits);
   void Visit(const BlockSet& blocks);
   int Fillout(double percents[], const int num_buckets);
  private:
-  const int page_bits_;
-  const int num_blocks_;
-  int num_visits_;
   std::vector<double> sum_ratios_;
+};
+
+class PageStatsVisitor : public PageVisitor {
+ public:
+  PageStatsVisitor(int page_bits);
+  void Visit(const BlockSet& blocks);
+  int Fillout(double percents[], const int num_buckets);
+ private:
+  struct PageStats { int blocks; int epochs; };
+  std::unordered_map<uint64_t, PageStats> pages;
 };
 
 // Implementations
 
-// DirtyRatioVisitor
+// PageVisitor
 
-DirtyRatioVisitor::DirtyRatioVisitor(int page_bits) :
-    page_bits_(page_bits), num_blocks_(1 << (page_bits - CACHE_BLOCK_BITS)),
-    sum_ratios_(num_blocks_, 0.0) {
-  assert(num_blocks_);
+PageVisitor::PageVisitor(int page_bits) :
+    page_bits_(page_bits), page_blocks_(1 << (page_bits - CACHE_BLOCK_BITS)) {
+  assert(page_blocks_);
   num_visits_ = 0;
 }
 
+void PageVisitor::Visit(const BlockSet& blocks) {
+  ++num_visits_;
+}
+
+// DirtyRatioVisitor
+
+DirtyRatioVisitor::DirtyRatioVisitor(int page_bits) :
+    PageVisitor(page_bits), sum_ratios_(page_blocks(), 0.0) {
+}
+
 void DirtyRatioVisitor::Visit(const BlockSet& blocks) {
+  PageVisitor::Visit(blocks);
   std::unordered_map<uint64_t, int> pages;
   // Count how many blocks are dirty in a page
   for (BlockSet::const_iterator it = blocks.begin();
       it != blocks.end(); ++it) {
-    pages[(it->first) >> (page_bits_ - CACHE_BLOCK_BITS)] += 1;
+    pages[(it->first) >> (page_bits() - CACHE_BLOCK_BITS)] += 1;
   }
   if (pages.empty()) return;
 
@@ -64,19 +94,18 @@ void DirtyRatioVisitor::Visit(const BlockSet& blocks) {
       it != pages.end(); ++it) {
     sum_ratios_[it->second - 1] += (double)1 / pages.size();
   }
-  ++num_visits_;
 }
 
 int DirtyRatioVisitor::Fillout(double percents[], const int n) {
-  assert(num_blocks_ % n == 0);
+  assert(page_blocks() % n == 0);
   for (int i = 0; i < n; ++i) percents[i] = 0.0;
-  if (num_visits_) {
-    int unit = num_blocks_ / n;
-    for (int i = 0; i < num_blocks_; ++i) {
-      percents[i / unit] += sum_ratios_[i] / num_visits_;
+  if (num_visits()) {
+    int unit = page_blocks() / n;
+    for (int i = 0; i < page_blocks(); ++i) {
+      percents[i / unit] += sum_ratios_[i] / num_visits();
     }
   }
-  return num_visits_;
+  return num_visits();
 }
 
 #endif // SEXAIN_EPOCH_VISITOR_H_
